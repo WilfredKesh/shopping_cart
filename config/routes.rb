dcf939962ea29dc67ef6187aefe26c9a6a74da74
Wilfred Kesh <wilfredkesh5@gmail.com>
2016-08-14 06:53:22 -0400

    scaffolded product model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  resources :products
+  root 'page#home'
+
+  get 'page/home'
+
+  get 'page/about'
+
+  get 'page/faq'
+
+  get 'page/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
